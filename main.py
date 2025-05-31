62534fb03ed651bfc1603d229e86cbe88b92b825
Fawaz Adeniji <fawazadeniji123@gmail.com>
2025-05-31 22:34:59 +0100

    Add CORS middleware to FastAPI application for improved cross-origin requests handling

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ import tempfile
 import face_recognition
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    # Configure CORS settings
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 @app.post("/encode-face")
